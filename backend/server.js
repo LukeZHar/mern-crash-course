6ccf4ef00ab56b7ad4911474195127a6e61a09c4
LukeZHar <luke.z.harris@hotmail.com>
2025-11-02 17:14:46 +1000

    feat: initialize frontend with Vite, React, and Chakra UI
    - Added package.json for project dependencies and scripts.
    - Created main application structure with App component.
    - Implemented Navbar component with routing and theme toggle.
    - Added HomePage and CreatePage components for navigation.
    - Configured Vite with vite.config.js for React support.

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on  http://localhost:5000");
+  console.log("Server is running on  http://localhost:" + PORT);
 });
