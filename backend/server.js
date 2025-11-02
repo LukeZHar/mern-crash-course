c1fed20228ad0b25cea04bfec8aa3329c40b8ff9
LukeZHar <luke.z.harris@hotmail.com>
2025-11-02 15:09:40 +1000

    Feat: implement product model and routes for CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.send("Hello from Express! Server is up and running.");
-});
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
