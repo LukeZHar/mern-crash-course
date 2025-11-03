8947a07af1bb39a01dba6835aca8a0c8d3d5f8b3
LukeZHar <luke.z.harris@hotmail.com>
2025-11-03 15:36:27 +1000

    feat: implement ProductCard component and integrate with HomePage for product display and deletion

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react"
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useProductStore } from "../store/product"
+import ProductCard from "../components/ProductCard"
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products)
+
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.xl"} py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+        <SimpleGrid 
+          column={{
+            base: 1,
+            md: 2,
+            lg: 3,
+            xl: 4,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+        {products.length === 0 && (
+          <Text fontSize="xl" textAlign={"center"} fontWeight="bold" color="gray.500">
+          No products found 😔 {" "}
+          <Link to={"/create"}>
+            <Text color="blue.500" _hover={{ textDecoration: "underline" }}>
+              Create A Product 🛍️
+            </Text>
+          </Link>
+        </Text>
+        )}
+      </VStack>
+    </Container>
   )
 }
 
